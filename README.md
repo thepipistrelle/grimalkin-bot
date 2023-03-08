70452a3472f0be4dee44b62080e0c7da7ae5ddbb
dee-fitz <122060680+dee-fitz@users.noreply.github.com>
2023-03-08 17:38:40 -0600

    grimalkin-bot first release - 0.0.0 grimalkin bot is a tarot reading bot that pulls one tarot card, finds its given keyword description, and giving you an embedded message with your reading's information on it. currently, the embed feature still needs a few tweaks for visual appeal, but funcitonally it is working.
    future plans:
    - have different reading layouts
    - use reaction commands to give further details on your reading
    - use given card pngs for your random cards in the embed
    
    that's all for the first version release of grimalkin-bot!

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # grimalkin-bot
 tarot bot.
+
+@grimalkin.command()
+async def ping(ctx):
+    embed = discord.Embed(colour=discord.Colour.green(), description="this is the description", title="this is the title")
+    
+    embed.set_footer(text="this is the footer")
+    embed.set_author(name=ctx.message.author, icon_url="https://tenor.com/bpfoU.gif")
+    embed.set_thumbnail(url="https://tenor.com/bpfoU.gif")
+    embed.set_image(url="https://tenor.com/bpfoU.gif")
+
+    embed.add_field(name="field 1 inline=f", value="https://tenor.com/bpfoU.gif", inline=False)
+    embed.add_field(name="field 2 inline=t", value="https://tenor.com/bpfoU.gif", inline=True)
+    embed.insert_field_at(1, name="embed.insert_field_at(1,...)", value="https://tenor.com/bpfoU.gif")
+
+    await ctx.send(embed=embed)
--- grimalkin.py
+++ grimalkin.py
@@ -18,25 +18,35 @@ async def change_status():
 
 #commands
 ##single tarot reading
-@grimalkin.command(aliases=["reading", "tarotreading"])
-async def tarot(ctx, *, question):
+@grimalkin.command()
+async def tarot(ctx, author, question):
     #open up our card file and our description file in read mode. then assign to new variables as split lines.
-    with open("card.txt", "r") as file1, open("description.txt") as file2:
-        description_lines = file2.read().splitlines()
-        card_lines = file1.read().splitlines()
-        #get the initial card
-        response_card = random.choice(card_lines)
-        card = str(response_card)
+    with open("card.txt", "r") as file1:
+        card_options=file1.read().splitlines()
+        response_card=random.choice(card_options)
+        card=str(response_card)
         print(card)
-        #get the description
-        for line in description_lines:
-            if any(description in line for description in description_lines):
-                print(line)
-        reading=str(line)
-    #send message with the full reading.      
-    await ctx.send(reading)
+    #compare card to description list
+    with open("description.txt", "r") as file2:
+        descriptions=file2.readlines()
+        for line in descriptions:
+            if line.startswith(card):
+                card_description=str(line)
+                reading=(card_description.split(':', 1)[-1])
+                print(reading)
 
-    ##
+                ##now time to set up our embed
+
+                embed = discord.Embed(colour=discord.Colour.green(), title="your tarot card", description="here is a keyword description of your card.")
+
+                embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
+
+                embed.add_field(name="your card:", value=card, inline=False)
+                embed.add_field(name="the card's keyword description:", value=reading, inline=False)
+                embed.set_image(url="https://drive.google.com/file/d/11V1aGl6VZX1L5r0PXEoy0mi6p7srHgTH/view?usp=share_link")
+                embed.set_footer(text="here are some reaction options")
+    #send message with the full reading.      
+                await ctx.send(embed=embed)
 
 #start-up loads
 @grimalkin.event 
@@ -47,6 +57,28 @@ async def on_ready():
 #final run --- do NOT put anything under here
 grimalkin.run(token)
 #####################################################################################
+#new random search
+#with open("card.txt", "r") as file1, open("description.txt") as file2:
+ #       description_lines = file2.read().splitlines()
+  #      card_lines = file1.read().splitlines()
+   #     #get the initial card
+    #    response_card = random.choice(card_lines)
+     #   card = str(response_card)
+      #  print(card)
 
 
 
+
+
+#            with open("card.txt", "r") as file1, open("description.txt") as file2:
+ #       description_lines = file2.read().splitlines()
+  #      card_lines = file1.read().splitlines()
+   #     #get the initial card
+    #    response_card = random.choice(card_lines)
+     #   card = str(response_card)
+      ##  print(card)
+        ###get the description
+        #for line in description_lines:
+           # if any(card in line for card in description_lines):
+         #       reading=str(card)
+          #      print(reading)
